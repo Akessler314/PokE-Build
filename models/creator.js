161d94a9bedc52a29a8de8884f4363d743fc93fb
Bounty556 <jacobmayday@gmail.com>
2020-06-09 07:40:45 -0600

    Added creator and pokemon tables to DB via Sequelize

---
+++ models/creator.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  const Creator = sequelize.define("Creator", {
+    username: DataTypes.STRING
+  });
+
+  Creator.associate = function(models) {
+    // Each creator can have many pokemon
+    Creator.hasMany(models.Pokemon, {
+      onDelete: "cascade"
+    });
+  };
+
+  return Creator;
+};
