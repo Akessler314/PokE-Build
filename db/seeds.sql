564dcc07dc84d2f0529015f368ece00396b177ac
Bounty556 <jacobmayday@gmail.com>
2020-06-10 19:27:09 -0600

    Fixed seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ USE dreamteam_db;
 
 INSERT INTO creators(username, createdAt, updatedAt) VALUES ("Ash", "2000-01-01 12:12:12", "2000-01-01 12:12:12");
 
-INSERT INTO pokemons(name, stats, moves, createdAt, updatedAt, CreatorId) VALUES ('Pikachu', '{"hp":50,"speed":50,"attack":50,"spAttack":50,"defense":50,"spDefense":50}', '{"move1":1,"move2":2,"move3":3,"move4":4}', "2000-01-01 12:12:12", "2000-01-01 12:12:12", 1);
+INSERT INTO pokemons(name, stats, moves, type1, type2, createdAt, updatedAt, CreatorId) VALUES ('Pikachu', '{"hp":50,"speed":50,"attack":50,"spAttack":50,"defense":50,"spDefense":50}', '{"move1":1,"move2":2,"move3":3,"move4":4}', 1, 2, "2000-01-01 12:12:12", "2000-01-01 12:12:12", 1);
