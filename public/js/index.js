22139ae53523a1e1ae9129a039ca81b871ff973c
LJ Spencer <hockeyduck3@gmail.com>
2020-06-11 21:15:08 -0600

    Got the search bar to show results underneath it

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+
 $('.searchBtn').on('click', event => {
   event.preventDefault();
 
@@ -5,5 +7,67 @@ $('.searchBtn').on('click', event => {
     url: '/api/pokemon/search/' + $('.searchBar').val()
   }).then(results => {
     console.log(results);
+
+    // Show an error if the there are no search results
+    if (results.length === 0) {
+      $('.error').text('Couldn\'t find any Pokemon with that name');
+      $('.error').slideDown('slow');
+      return;
+    }
+
+    // Go through each of the pokemon from the search and add them to the list
+    results.forEach(pokemon => {
+      const div = $('<div>').addClass('col-lg-3');
+
+      const link = $('<a>').attr('href', `/pokemon/${pokemon.searchableName}`);
+
+      const img = $('<img>')
+        .attr({
+          src: 'https://via.placeholder.com/150',
+          alt: `Picture of ${pokemon.name}`
+        })
+        .addClass('resultImg');
+
+      const card = $('<div>').addClass('card resultCard');
+
+      const cardBody = $('<div>').addClass('card-body');
+
+      const text = $('<p>').text(`Name: ${pokemon.name}`).addClass('resultText');
+
+      div.append(card);
+      card.append(link);
+      link.append(cardBody);
+      cardBody.append(img);
+      cardBody.append(text);
+
+      $('.searchResults').append(div);
+    });
+
+    // Show the results
+    $('.searchResults').slideDown('slow');
   });
+});
+
+$('.searchBar').on('keydown', key => {
+  $('.searchResults').slideUp('slow');
+
+  if (key.keyCode === 8) {
+
+    if ($('.searchBar').val().trim() === '') {
+      $('.buttonRow').slideDown('slow');
+      $('.searchResults').empty();
+    }
+
+    $('.error').slideUp('slow');
+  } else {
+    $('.buttonRow').slideUp('slow');
+    $('.searchResults').empty();
+  }
+});
+
+$(document).click(() => {
+  if ($('.searchBar').val().trim() === '') {
+    $('.searchResults').slideUp('slow');
+    $('.buttonRow').slideDown('slow');
+  }
 });
