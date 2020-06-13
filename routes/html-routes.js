ce1799d8dc6befbc1efe9ab309410ca799f98360
Bounty556 <jacobmayday@gmail.com>
2020-06-13 12:20:56 -0600

    Added Index route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,3 +1,5 @@
+const Pokemon = require('../controllers/pokemonController');
+
 module.exports = function(app) {
   app.get('/pokemon/battle/:id1/:id2', (req, res) => {
     res.render('battle', {
@@ -12,5 +14,14 @@ module.exports = function(app) {
 
   app.get('/pixel', (req, res) => {
     res.render('pixel');
+  });
+
+  app.get('/pokemon/index/:page', (req, res) => {
+    Pokemon.findAllInOrder('updatedAt', 'DESC', req.params.page).then(
+      results => {
+        console.log(results);
+        res.render('view-all-pokemon', { pokemon: results });
+      }
+    );
   });
 };
