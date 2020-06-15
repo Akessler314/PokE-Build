9e9bdef08bed8a51c4c475dec1805529711efa2d
Andrew Kessler <andrew.kessler95@gmail.com>
2020-06-15 10:57:50 -0600

    added function to save the user inputted stats as an object

--- public/js/pixel.js
+++ public/js/pixel.js
@@ -1,12 +1,22 @@
 const $colorPicker = $('#colorPicker');
 //code for the pokemon stats
-// let avaialblePoitns = 100;
-// let $hp = $('#hp');
-// let $speed = $('#speed');
-// let $defense = $('#defense');
-// let $spDefense = $('#SP_Defense');
-// let $attack = $('#attack');
-// let $spAttack = $('#SP_Attack');
+// const avaialblePoitns = 100;
+const $hp = $('#hp');
+const $speed = $('#speed');
+const $defense = $('#defense');
+const $spDefense = $('#SP_Defense');
+const $attack = $('#attack');
+const $spAttack = $('#SP_Attack');
+
+//prottype object for pokemon stat object
+const stats = {
+  hp: 0,
+  speed: 0,
+  defense: 0,
+  spDefense: 0,
+  attack: 0,
+  spAttack: 0
+};
 
 $(document).ready(() => {
   addCellClickListener();
@@ -26,4 +36,15 @@ function addCellClickListener() {
 // Toggles grid opacity
 $('#toggle').click(() => {
   $('table tr td').toggleClass('opacity');
+});
+
+$('#statSubmit').click(() => {
+  const newPokeStats = Object.create(stats);
+  newPokeStats.hp = $hp.val();
+  newPokeStats.speed = $speed.val();
+  newPokeStats.defense = $defense.val();
+  newPokeStats.spDefense = $spDefense.val();
+  newPokeStats.attack = $attack.val();
+  newPokeStats.spAttack = $spAttack.val();
+  console.log(newPokeStats);
 });
