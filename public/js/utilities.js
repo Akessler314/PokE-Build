f9ebf01899621b60f006504aaf6ef77aee65b664
Bounty556 <jacobmayday@gmail.com>
2020-06-15 20:38:11 -0600

    Added basic UI for battle sim

--- public/js/utilities.js
+++ public/js/utilities.js
@@ -22,5 +22,16 @@ const DATA = {
     'dragon',
     'dark',
     'fairy'
-  ]
+  ],
+  battleOptions: ['Attack', 'Info']
 };
+
+function formatMoveName(move) {
+  const words = move.split('-');
+
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+  }
+
+  return words.join(' ');
+}
