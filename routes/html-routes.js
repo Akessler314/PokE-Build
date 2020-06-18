8e0d94179599b835dfc37a97cbc0b40184bff538
Bounty556 <jacobmayday@gmail.com>
2020-06-17 21:38:31 -0600

    Added some documentation to api routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,18 @@
 const Pokemon = require('../controllers/pokemonController');
 
 module.exports = function(app) {
+  // The battle sim page
   app.get('/pokemon/battle/:id1/:id2', (req, res) => {
+    // Main index page
+    app.get('/', (req, res) => {
+      res.render('index');
+    });
+
     // Check to see if the user is logged in and id1 is their pokemon
     if (!req.user) {
       res.redirect('/login');
     } else if (req.user.id !== req.params.id1) {
-      res.status(401).end();
+      res.redirect('/');
     } else {
       res.render('battle', {
         pokemon1: req.params.id1,
@@ -15,10 +21,7 @@ module.exports = function(app) {
     }
   });
 
-  app.get('/', (req, res) => {
-    res.render('index');
-  });
-
+  // Pokemon creation page
   app.get('/pixel', (req, res) => {
     // Check to see if the user is logged in
     if (!req.user) {
@@ -28,6 +31,7 @@ module.exports = function(app) {
     }
   });
 
+  // Pokemon index page
   app.get('/pokemon/index/:page', (req, res) => {
     Pokemon.findAllInOrder('updatedAt', 'DESC', req.params.page).then(
       results => {
@@ -36,6 +40,7 @@ module.exports = function(app) {
     );
   });
 
+  // Signup page
   app.get('/signup', (req, res) => {
     if (req.user) {
       res.redirect('/');
@@ -44,6 +49,7 @@ module.exports = function(app) {
     }
   });
 
+  // Login page
   app.get('/login', (req, res) => {
     if (req.user) {
       res.redirect('/');
